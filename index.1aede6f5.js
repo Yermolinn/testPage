4874ac5fef4ed3fc278c5580a58335d1984aca24
Yermolinn <111793261+Yermolinn@users.noreply.github.com>
2023-06-09 21:03:36 +0000

    Deploying to gh-pages from @ Yermolinn/testPage@f8e1063b87b8fb28ff5a737fb441b3dbb146c7ec 🚀

---
+++ index.1aede6f5.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".burger"),o=document.querySelector(".header-mob"),t=document.querySelector("body");console.log(e),e.addEventListener("click",(function(e){this.classList.toggle("burger_active"),o.style.display="none"===o.style.display?"block":"none",t.classList.toggle("no-scroll")}))}();
+//# sourceMappingURL=index.1aede6f5.js.map
---
+++ index.374e31d6.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".burger"),o=document.querySelector(".header-mob"),t=document.querySelector("body");console.log(e),e.addEventListener("click",(function(e){this.classList.toggle("burger_active"),o.style.display="none"===o.style.display?"block":"none",t.classList.toggle("no-scroll")}));
+//# sourceMappingURL=index.374e31d6.js.map
